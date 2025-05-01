5c5e42ce7b4a1669de69e8b75276a3b5f804274c
akpatel-1 <akpatel.work1@gmail.com>
2025-05-01 22:08:54 +0530

    Arguments in Java

--- Java DSA/Argument.java
+++ Java DSA/Argument.java
@@ -1,7 +1,9 @@
 public class Argument {
     public static void main(String[] args) {
+        String temp="";
         for (int i = 0; i < args.length; i++) {
-            System.out.println("Your input is: " + args[i]);
+            temp = temp + " " + args[i];
         }
+        System.out.println("Your input is :" + temp);
     }
 }
