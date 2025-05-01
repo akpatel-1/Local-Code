30e87fdc629118fabb114ad43b935cd527375313
akpatel-1 <akpatel.work1@gmail.com>
2025-05-01 21:58:12 +0530

    Variables in java

--- Java DSA/Variable.java
+++ Java DSA/Variable.java
@@ -2,10 +2,12 @@ public class Variable{
     public static void main(String args[]){
         int a = 10;
         int b = 20;
+        final int c = 50;
         String name = "Akash patel";
         System.out.print(a + "\n" + b + "\n" + name + "\n");
         a = 60;
         b = 50;
+        //c = 55 Compile-time error
         name = "patel";
         System.out.print(a + "\n" + b + "\n" + name + "\n");
     }
