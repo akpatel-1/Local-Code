11284853729513a9fbf490089a0973aa1f2470f0
akpatel-1 <akpatel.work1@gmail.com>
2025-05-01 21:51:23 +0530

    Practise question

--- Java DSA/Print.java
+++ Java DSA/Print.java
@@ -1,5 +1,5 @@
 public class Print{
     public static void main(String args[]){
-        System.out.print("* * * * \n* * *\n* *\n*\n");
+        System.out.print("* * * *\n* * *\n* *\n*\n");
     }
 }
