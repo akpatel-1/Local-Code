27eef4ca3f7ee1b429eb67a62a0f14a95150bdd7
akpatel-1 <akpatel.work1@gmail.com>
2025-05-02 19:50:10 +0530

    Practise Question

---
+++ Java DSA/Sum.java
@@ -0,0 +1,11 @@
+public class Sum{
+    public static void main(String[] args){
+        int a = 55 , b = 45;
+        System.out.println("Sum of a & b is " + (a+b));
+    }
+}
+
+// LEARNING
+//In Java, if either side of the + operator is a String, Java treats the whole expression as a string concatenation.
+
+//This is Java's operator overloading: the + operator is used for both addition and string joining, depending on the types involved.
