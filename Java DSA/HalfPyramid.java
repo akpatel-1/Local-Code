7586e7e52b4082fc4648ac745517ac12a783eed9
akpatel-1 <akpatel.work1@gmail.com>
2025-05-03 11:35:45 +0530

    Half Pyramid pattern

--- Java DSA/HalfPyramid.java
+++ Java DSA/HalfPyramid.java
@@ -2,7 +2,7 @@ public class HalfPyramid{
     public static void main(String[] args){
         for(int i=0 ; i<5 ; i++){
             for(int j=0 ; j<=i ; j++){
-                System.out.print( j+1 + " ");
+                System.out.print(j+1 + " ");
             }
             System.out.print("\n");
         }
