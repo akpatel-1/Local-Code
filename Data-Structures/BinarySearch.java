88634ecf7cf7474e1c41f4f20e0e084a79782fe7
akpatel-1 <akpatel.work1@gmail.com>
2025-05-05 04:35:43 +0530

    Searching in array

--- Data-Structures/BinarySearch.java
+++ Data-Structures/BinarySearch.java
@@ -1,7 +1,6 @@
-
 import java.util.Scanner;
 
-public class LinearSearch {
+public class BinarySearch {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -10,7 +9,7 @@ public class LinearSearch {
 
         int[] array = new int[size];
 
-        System.out.print("Enter " + size + " numbers in the array separated by space: ");
+        System.out.print("Enter " + size + " sorted numbers in the array separated by space: ");
         for (int i = 0; i < array.length; i++) {
             array[i] = scan.nextInt();
         }
@@ -18,7 +17,7 @@ public class LinearSearch {
         System.out.print("Enter the number you want to search: ");
         int key = scan.nextInt();
 
-        int index = getLinearSearch(array, key);
+        int index = getBinarySearch(array, key);
         if (index >= 0) {
             System.out.println(key + " found at index number: " + index);
         } else {
@@ -28,16 +27,20 @@ public class LinearSearch {
         scan.close(); 
     }
 
-    public static int getLinearSearch(int[] array, int key) {
-        for (int i = 0; i < array.length; i++) {
-            if (key == array[i]) {
-                return i;
+    public static int getBinarySearch(int[] array, int key) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+            if (array[mid] == key) { 
+                return mid; 
+            } else if (array[mid] < key) { 
+                low = mid + 1;
+            } else { 
+                high = mid - 1;
             }
         }
-        return -1;
+        return -1; 
     }
 }
-
-/*
-LEARNING
-The Scanner reads the next integer token from the input, and it treats any whitespace (including spaces, tabs, and newlines) as delimiters (separators) between tokens by default. */
