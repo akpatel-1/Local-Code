0ce795e83653ef67ee0cf1791641299e895fa660
akpatel-1 <akpatel.work1@gmail.com>
2025-05-04 21:30:07 +0530

    Largest Number in array

--- Data-Structures/LargestNumber.java
+++ Data-Structures/LargestNumber.java
@@ -1,10 +1,12 @@
+import java.util.*;
 public class LargestNumber{
     public static void main(String[] main){
-        int max = 0;
-        int[] array = {6, 4, 8, 9, 0, 1, 2, 3};
-        for(int i=0; i<array.length; i++){
-            max = Math.max(max, array[i]);
+        int max = Integer.MIN_VALUE;
+        int[] array = {6, 4, 8, 1, 0, 1, 2, 3};
+        for (int num : array) {
+            max = Math.max(max, num);
         }
+
         System.out.println("Largest number in array is " + max);
     }
 }
