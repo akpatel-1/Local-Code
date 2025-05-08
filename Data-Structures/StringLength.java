abde06a840d45bfb25973ec200a15cafb9a771f2
akpatel-1 <akpatel.work1@gmail.com>
2025-05-08 11:02:02 +0530

    String Concatenate

--- Data-Structures/StringLength.java
+++ Data-Structures/StringLength.java
@@ -1,5 +1,6 @@
 public class StringLength{
     public static void main(String[] args){
-        
+        String str = "I love my country India";
+        System.out.println("Length is : " + str.length());
     }
 }
