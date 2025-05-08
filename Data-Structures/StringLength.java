33c64496331cccd8944a20c878feff4bbd3ea1a6
akpatel-1 <akpatel.work1@gmail.com>
2025-05-08 11:03:46 +0530

    String Length

--- Data-Structures/StringLength.java
+++ Data-Structures/StringLength.java
@@ -1,6 +1,6 @@
 public class StringLength{
     public static void main(String[] args){
-        String str = "I love my country India";
+        String str = "I love my country India ";
         System.out.println("Length is : " + str.length());
     }
 }
