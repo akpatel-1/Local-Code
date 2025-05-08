473fb94315dc5642d98e11c38fa8677398577ea4
akpatel-1 <akpatel.work1@gmail.com>
2025-05-08 11:36:01 +0530

    Palindrome Question

--- Java DSA/Data-Structures/Array/Palindrome.java
+++ Java DSA/Data-Structures/Array/Palindrome.java
@@ -1,5 +1,20 @@
-public class Palindrome{
-    public static void main(String[] args){
-        
+import java.util.*;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = scan.next();
+        System.out.println("is " + str + " palindrome: " + isPalindrome(str));
+    }
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for(int i=0; i<=n/2; i++){
+            if(str.charAt(i)==str.charAt(n-i-1)){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
 }
