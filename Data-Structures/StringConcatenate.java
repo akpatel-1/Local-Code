7546877539124a836e54432ae955968fea36e46b
akpatel-1 <akpatel.work1@gmail.com>
2025-05-08 11:05:30 +0530

    String Concatenate

---
+++ Data-Structures/StringConcatenate.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class StringConcatenate {
+    public static void main(String[] args) {
+        String firstName = "Akash";
+        String lastName = "Patel";
+        System.out.println(firstName + " " + lastName);
+    }
+}
