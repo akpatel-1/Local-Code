28bedcdb830fef9a56a89e22bfee3299075beffb
akpatel-1 <akpatel.work1@gmail.com>
2025-05-08 11:20:07 +0530

    charAt method in java

--- Java DSA/Data-Structures/Array/CharAt.java
+++ Java DSA/Data-Structures/Array/CharAt.java
@@ -1,10 +1,12 @@
 import java.util.*;
+
 public class CharAt {
     public static void main(String[] args) {
         String str = "I love my country and I am proud of it.";
         System.out.print("Letters : ");
-        for(int i=0; i<str.length(); i++){
-           System.out.print(i + str.charAt(i) + " ");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(i + "-" + str.charAt(i) + " ");
         }
+        System.out.println("length of string: " + str.length());
     }
 }
