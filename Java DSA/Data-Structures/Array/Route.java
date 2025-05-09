3bb5dabade0b552835683a8c101c1035f9d824f6
akpatel-1 <akpatel.work1@gmail.com>
2025-05-09 20:05:06 +0530

    Route question

--- Java DSA/Data-Structures/Array/Route.java
+++ Java DSA/Data-Structures/Array/Route.java
@@ -6,7 +6,21 @@ public class Route {
         String str = scan.next();
         System.out.println("Shorted route: " + getShortRoute(str));
     }
-    public static int getShortRoute(String str){
-        
+    public static double getShortRoute(String str) {
+    int x = 0, y = 0;
+    for (int i = 0; i < str.length(); i++) {
+        char ch = str.charAt(i);
+        if (ch == 'E') ++x;
+        else if (ch == 'W') --x;
+        else if (ch == 'N') ++y;
+        else if (ch == 'S') --y;
+        else return -1;
     }
+    return Math.sqrt((x * x) + (y * y));
 }
+
+}
+
+// In Java, the method signature for Math.sqrt() is:
+// public static double sqrt(double a)
+// That means no matter what you pass in, Math.sqrt() returns a double.
