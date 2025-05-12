8d9122dc926ed67d59ec330891ea522b57ad9e1f
akpatel-1 <akpatel.work1@gmail.com>
2025-05-12 11:11:19 +0530

    Array & Strings in java

--- Java DSA/Data-Structures/Array/StringConcatenate.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class StringConcatenate {
-    public static void main(String[] args) {
-        String firstName = "Akash";
-        String lastName = "Patel";
-        System.out.println(firstName + " " + lastName);
-    }
-}
--- Java DSA/Data-Structures/Array/StringLength.java
+++
@@ -1,6 +0,0 @@
-public class StringLength{
-    public static void main(String[] args){
-        String str = "I love my country India ";
-        System.out.println("Length is : " + str.length());
-    }
-}
